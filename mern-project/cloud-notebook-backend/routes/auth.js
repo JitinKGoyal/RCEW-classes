56442083f01286d416fb802419735947c0467f3d
JitinKGoyal <jkgoyal85@gmail.com>
2022-12-15 09:45:36 +0530

    signUp api

--- mern-project/cloud-notebook-backend/routes/auth.js
+++ mern-project/cloud-notebook-backend/routes/auth.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
+const signUpValidations = [
+    body('name', "Name must be minimum of 2 chars").isLength({ min: 2 }),
+    body('email', "Enter a valid email").isEmail(),
+    body('password', "Paswword must be minimum of 5 chars").isLength({ min: 5 })]
 
 // For Sign Up
-router.get('/', (req, res) => {
+router.get('/', signUpValidations, async (req, res) => {
 
-    // console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    
-    res.send(req.body)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User.findOne({ email: req.body.email }); // 2sec
+
+    if (user) {
+        return res.json({ error: "user already exists" });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user));
+
+    // const user = User(req.body);
+    // user.save();
 });
 
 
